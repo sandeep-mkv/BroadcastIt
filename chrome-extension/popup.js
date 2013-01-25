98a7b86300150eff52ae4bd467b983d044982c4a
Abhilash <abhilash.m@multunus.com>
2013-01-26 05:13:49 +0530

    Run the extension in background

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,38 +1,33 @@
-    
+broadcastUrl = "http://www.google.com";
+socket = io.connect("http://192.168.0.190:4000");
+browser = {
+    id : 0
+};
 
-	var broadcastUrl = "http://www.google.com";
-    var socket = io.connect("http://192.168.0.190:4000");
-    var browser = {
-    	id : 0
-    };
+socket.on('connect', function() {
+    	      socket.emit('client_connected', browser);
+          });
 
-    socket.on('connect', function() {
-    	socket.emit('client_connected', browser);
-    });
+socket.on('accepted', function(data) {
+    	      browser.id = data.id;
+          });
 
-    socket.on('accepted', function(data) {
-    	browser.id = data.id;
-    });
-
-    socket.on('open_url', function(url) {
-    	console.log('open url');
-       	chrome.tabs.create({url: url});
-    });
+socket.on('open_url', function(url) {
+    	      console.log('open url');
+       	      chrome.tabs.create({url: url});
+          });
 
 
+chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+		  function(tabs){
+		      broadcastUrl = tabs[0].url;
+		  });	
 
-$(document).ready(function(){
-
-    chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-		function(tabs){
-			broadcastUrl = tabs[0].url;
-		}
-	);	
-
-    $('.broadcast').click(function(){
+chrome.browserAction.onClicked.addListener(
+    function(){
     	console.log('broadcast click');
     	socket.emit('broadcast', broadcastUrl);
-    })
+    });
+
 	
-});
 
